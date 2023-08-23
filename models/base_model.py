3838f14f3609dcb351205eb9048727bff0837245
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-08-24 02:08:44 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Define BaseModel class"""
-from sqlalchemy import Integer, String, Column, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+'''
+    This module defines the BaseModel class
+'''
+from os import getenv
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
@@ -16,7 +17,6 @@ class BaseModel:
     '''
         Base class for other classes to be used for the duration.
     '''
-
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
@@ -30,22 +30,21 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            if kwargs.get('created_at'):
+            if kwargs.get("created_at"):
                 kwargs["created_at"] = datetime.strptime(
                     kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
             else:
                 self.created_at = datetime.now()
-            if kwargs.get('updated_at'):
+            if kwargs.get("created_at"):
                 kwargs["updated_at"] = datetime.strptime(
                     kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
             else:
                 self.updated_at = datetime.now()
-            if not self.id:
-                self.id = str(uuid.uuid4())
-
             for key, val in kwargs.items():
                 if "__class__" not in key:
                     setattr(self, key, val)
+            if not self.id:
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         '''
@@ -77,12 +76,12 @@ class BaseModel:
         cp_dct['__class__'] = self.__class__.__name__
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        cp_dct.pop('_sa_instance_state', None)
-
+        if hasattr(self, "_sa_instance_state"):
+            del cp_dct["_sa_instance_state"]
         return (cp_dct)
 
     def delete(self):
-        """
-        Deletes the current instance from models.storage by calling delete
-        """
+        '''
+            Deletes an object
+        '''
         models.storage.delete(self)
