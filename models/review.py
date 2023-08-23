12bf3cd3be41db2a5b331c7bcfea83318b43f616
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-08-23 22:58:12 +0300

    basil

--- models/review.py
+++ models/review.py
@@ -2,17 +2,22 @@
 """
 Module Review class
 """
-from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
-    """
-    Inherits from BaseModel
-    Public class attributes:
-        place_id:            (str) will be Place.id
-        user_id:             (str) will be User.id
-        text:                (str)
-    """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    '''
+        Implementation for the Review.
+    '''
+    __tablename__ = "reviews"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
--- models/state.py
+++ models/state.py
@@ -2,13 +2,36 @@
 """
 Module State class
 """
-from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import models
 
 
-class State(BaseModel):
-    """
-    Inherits from BaseModel
-    Public class attribute:
-        name: (str)
-    """
-    name = ""
+class State(BaseModel, Base):
+    '''
+        Implementation for the State.
+        Create relationship between class State (parent) to City (child)
+    '''
+    __tablename__ = "states"
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            '''
+                Return list of city instances if City.state_id==current
+                State.id
+                FileStorage relationship between State and City
+            '''
+            list_cities = []
+            for city in models.storage.all("City").values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
