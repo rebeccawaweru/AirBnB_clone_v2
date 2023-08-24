ac17e59a2366b7e781a474d399dbcd4a34c397d6
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-08-24 03:16:49 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,13 +3,13 @@
     Implementation of the Amenity class
 '''
 from os import getenv
-from models.base_model import BaseModel, Base
+from base_model import BaseModel, Base
 from models.place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     '''
         Implementation for the Amenities.
     '''
