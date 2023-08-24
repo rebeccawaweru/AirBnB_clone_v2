f31bdb45bdbae0b17ee55d8ab2700926b8020b65
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-08-24 03:17:50 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,7 @@
 '''
 from os import getenv
 from base_model import BaseModel, Base
-from models.place import place_amenity
+from place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
