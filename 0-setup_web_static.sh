736663e52b5da9cf1e1931dd922101f2a7207f97
rebeccawaweru <wawerur95@gmail.com>
2023-09-07 21:45:02 +0300

    shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ mkdir -p /data/web_static/releases/test/
 touch /data/web_static/releases/test/index.html
 ln -fs /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-echo "<html>\n\t<head>\n\t</head>\n\t\t<body>\n\t\t\tHoberlton School\n\t\t</body>\n</html>" > /data/web_static/releases/test/index.html
+printf "<html>\n\t<head>\n\t</head>\n\t\t<body>\n\t\t\tHoberlton School\n\t\t</body>\n</html>" > /data/web_static/releases/test/index.html
 sed -i "s/# Only/\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/; \n\t\tautoindex off;\n\t}\n \n\t# Only/" /etc/nginx/sites-available/default
 sudo service nginx restart
